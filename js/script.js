3c9402a80ee870530570dfe6f745a74adb5cacd9
whywesmurfing <raymond.doan94@gmail.com>
2021-05-25 10:48:52 +1000

    Added wrapper for content and toggle button to show/hide the about me section

--- js/script.js
+++ js/script.js
@@ -4,7 +4,23 @@ window.onbeforeunload = function () {
 
 const menuIcon = document.getElementById("menu-icon")
 const menuList = document.querySelector(".menu-list")
+const readMore = document.getElementById("read-more")
+const aboutMeSection = document.querySelector(".about")
+
+let toggle = false;
 
 menuIcon.addEventListener("click", (e) => {
   menuList.classList.toggle("open")
 })
+
+readMore.addEventListener("click", (e) => {
+
+  aboutMeSection.classList.toggle("open-about");
+  if (!toggle) {
+    readMore.textContent = "Yay!";
+    toggle = true;
+  } else {
+    readMore.textContent = "About Me";
+    toggle = false;
+  }
+});
